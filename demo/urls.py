da73004829520e47da2505eb76061ba76c9c9e4c
srikanthpragada <srikanthpragada@gmail.com>
2018-04-12 21:04:59 +0530

    12-APR

--- demo/urls.py
+++ demo/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from demo.views import hello, wish, course
+from demo.views import hello, wish, course, list_courses, prime
 
 urlpatterns = [
     path('hello/', hello),
     path('wish/', wish),
     path('course/', course),
+    path('listcourses/', list_courses),
+    path('prime/', prime),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from demo.models import Course
+from datetime import datetime
+import math
 
 
 # Create your views here.
@@ -14,9 +16,37 @@ def hello(request):
 
 
 def wish(request):
-    return HttpResponse("Wishing...")
+    ct = datetime.now()
+    if ct.hour < 12:
+        msg = "Good Morning!"
+    elif ct.hour < 17:
+        msg = "Good Afternoon!"
+    else:
+        msg = "Good Evening!"
+
+    return HttpResponse("<h1>{}</h1>".format(msg))
 
 
 def course(request):
     c = Course("Angular", 3000)
-    return HttpResponse("<h2>%s</h2>" % (str(c)))
+    return render(request, 'demo/course.html', {"course": c})
+
+
+def list_courses(request):
+    courses = [Course("Angular", 3000), Course("Python", 6000), Course("Java EE", 8000)]
+    return render(request, 'demo/list_courses.html', {"courses": courses})
+
+
+def prime(request):
+    if request.method == "GET":
+        return render(request, 'demo/prime.html', {"message", ""})
+    else:
+        # read data and process
+        num = int(request.POST["num"])  # convert string to int
+        message = "A Prime Number"
+        for i in range(2, math.sqrt(num) + 1):
+            if num % i == 0:
+                message = "Not A Prime Number"
+                break
+
+        return render(request, 'demo/prime.html', {"message": message})
