9957a6987abfeea1d73b1d095491f184567f3446
srikanthpragada <srikanthpragada@gmail.com>
2018-04-27 20:59:07 +0530

    27-APR

--- demo/models.py
+++ demo/models.py
@@ -1,6 +1,26 @@
+# Create your models here.
+
 from django.db import models
 
-# Create your models here.
+
+# DBCourse class
+class DBCourse(models.Model):
+    title = models.CharField(max_length=20)
+    duration = models.IntegerField()
+    fee = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class DBStudent(models.Model):
+    fullname = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    course = models.ForeignKey('DBCourse', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.fullname
+
 
 class Course:
     def __init__(self, name, fee):
@@ -12,20 +32,16 @@ class Course:
 
 
 class Movies:
-     movies = {'Vizag' : ['Movie1','Movie2'],
-               'Chennai' : ['Movie3','Movie4'],
-               'Hyderabad' : ['Movie10', 'Movie11'],
-               'Mumbai': ['Movie8', 'Movie9','Movie6']
-               };
+    movies = {'Vizag': ['Movie1', 'Movie2'],
+              'Chennai': ['Movie3', 'Movie4'],
+              'Hyderabad': ['Movie10', 'Movie11'],
+              'Mumbai': ['Movie8', 'Movie9', 'Movie6']
+              };
 
-     @staticmethod
-     def get_cities():
-         return  Movies.movies.keys()
+    @staticmethod
+    def get_cities():
+        return Movies.movies.keys()
 
-     @staticmethod
-     def get_movies(city):
-         return Movies.movies[city]  # list of movies in the given city
-
-
-
-
+    @staticmethod
+    def get_movies(city):
+        return Movies.movies[city]  # list of movies in the given city
